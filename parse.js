f554df0ecf106b79ec497123b58d5c3ed89e1a27
Gryphon Myers <gryphon.myers@2k.com>
2017-10-04 16:44:07 -0700

    use innerhtml property to better handle html entities

--- parse.js
+++ parse.js
@@ -8,7 +8,7 @@ module.exports = function (h, html) {
     parser.readTokens({
         chars: function (tok) {
             if (current) {
-                current.children.push(tok.text);
+                current.innerHTML += tok.text;
             } else {
                 nodes.push(tok.text);
             }
@@ -23,12 +23,12 @@ module.exports = function (h, html) {
                     nodes.push(node);
                 }
             } else {
-                current = {tok: tok, parent: current, children:[]};
+                current = {tok: tok, innerHTML: '', parent: current, children:[]};
             }
         },
         endTag: function (tok){
             //TODO add support for SVG
-            var node = h(current.tok.tagName, {attributes: Object.assign({}, current.tok.attrs, current.tok.booleanAttrs)}, current.children);
+            var node = h(current.tok.tagName, {innerHTML: current.innerHTML, attributes: Object.assign({}, current.tok.attrs, current.tok.booleanAttrs)}, current.children);
             current = current.parent;
             if (!current) {
                 nodes.push(node);
